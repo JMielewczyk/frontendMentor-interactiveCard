8d58f3a20028cab188a84f720e0e992e6885bbf3
Jakub <jkbmielewczyk@gmail.com>
2022-09-05 18:56:41 +0200

    Update

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 const inputs = [...document.querySelectorAll('[data-input]')]
 const cardInfo = document.querySelectorAll('[data-index]')
 const wrongFormulas = document.querySelectorAll('.wrongFormula')
+const btns = document.querySelectorAll('button')
+const cardWrapper = document.querySelector('.cardWrapper')
+const completeState = document.querySelector('.completeState')
 
-let flag = false;
 inputs.forEach(input => input.addEventListener('keyup', (e) => {
     let inputNumber = input.dataset.input;
     let inputValue = e.target.value
-
+    let flag = false;
 
     if (inputNumber == 1 && /^[a-zA-Z ]+$/.test(inputValue) || inputNumber == 1 && inputValue == '') {
-        document.querySelector(`[data-index='${inputNumber}']`).textContent = inputValue
+        cardInfo[inputNumber].textContent = inputValue.toUpperCase()
         if (inputValue == '') {
             flag = false
-
+            cardInfo[inputNumber].textContent = 'JANE APPLESEED'
         }
     } else if (inputNumber == 1 && !(/^[a-zA-Z ]+$/.test(inputValue))) {
         inputs[inputNumber - 1].style.outline = '1px solid red'
         flag = true
     } else if (inputNumber == 2) {
         if (Number(inputValue) || inputValue == '') {
-            document.querySelector(`[data-index='${inputNumber}']`).textContent = inputValue
+            cardInfo[inputNumber - 2].textContent = inputValue
 
             if (inputValue == '') {
-                document.querySelector(`[data-index='${inputNumber}']`).textContent = '0000 0000 0000 0000'
+                cardInfo[inputNumber - 2].textContent = '0000 0000 0000 0000'
                 flag = false
             }
         } else {
@@ -30,26 +32,41 @@ inputs.forEach(input => input.addEventListener('keyup', (e) => {
             flag = true
         }
     } else if (inputNumber == 3) {
+        if (Number(inputValue) <= 12 && Number(inputValue) > 9 || inputValue == '') {
+            cardInfo[inputNumber].textContent = inputValue
+            flag = false
+        } else if (Number(inputValue) <= 9) {
+            cardInfo[inputNumber].textContent = '0' + inputValue
+        } else {
+            flag = true
+        }
+
+        if (inputValue == '') {
+            cardInfo[inputNumber].textContent = '00'
+        }
+    } else if (inputNumber == 4) {
+        if (Number(inputValue) < 40 && Number(inputValue) > 9 || inputValue == '') {
+            cardInfo[inputNumber - 2].textContent = inputValue
+            flag = false
+        } else if (Number(inputValue) <= 9) {
+            cardInfo[inputNumber - 2].textContent = '0' + inputValue
+        } else {
+            flag = true
+        }
+
+        if (inputValue == '') {
+            cardInfo[inputNumber - 2].textContent = '00'
+        }
+    } else if (inputNumber == 5) {
         if (Number(inputValue) || inputValue == '') {
-            document.querySelector(`[data-index='${inputNumber}']`).textContent = inputValue
+            cardInfo[inputNumber - 1].textContent = inputValue
             flag = false
         } else {
             flag = true
         }
 
-    } else if (inputNumber == 4) {
-        if (Number(inputValue) || inputValue == '') {
-            document.querySelector(`[data-index='${inputNumber - 1}']`).textContent = inputValue
-            flag = false
-        } else {
-            flag = true
-        }
-    } else if (inputNumber == 5) {
-        if (Number(inputValue) || inputValue == '') {
-            document.querySelector(`[data-index='${inputNumber}']`).textContent = inputValue
-            flag = false
-        } else {
-            flag = true
+        if (inputValue == '') {
+            cardInfo[inputNumber - 1].textContent = '000'
         }
     }
 
@@ -84,4 +101,10 @@ inputs.forEach(input => input.addEventListener('keyup', (e) => {
 
 
 
+}))
+
+
+btns.forEach(btn => btn.addEventListener('click', () => {
+    cardWrapper.classList.toggle('noActive')
+    completeState.classList.toggle('noActive')
 }))
