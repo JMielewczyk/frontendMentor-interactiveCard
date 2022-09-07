afcf7b09a7d218836ff6afcf2a5432f6ad385c14
Jakub <jkbmielewczyk@gmail.com>
2022-09-07 14:47:08 +0200

    Update

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const completeState = document.querySelector('.completeState')
 let inputValue;
 let emptyInputs = [];
 let flag;
+let nameLength;
+let cardNumberLength;
+let cvcLength;
 
 
 inputs.forEach(input => input.addEventListener('keyup', (e) => {
@@ -15,10 +18,12 @@ inputs.forEach(input => input.addEventListener('keyup', (e) => {
     let inputTarget = e.target
     flag = false;
     inputTarget.nextElementSibling.textContent = 'Wrong format'
+    userDate = Number(inputs[2].value) + Number(inputs[3].value)
 
 
     switch (Number(inputNumber)) {
         case 1:
+            nameLength = inputValue.length;
             if (/^[a-zA-Z ]+$/.test(inputValue) || !inputValue) {
                 cardText[inputNumber].textContent = inputValue.toUpperCase()
                 if (!inputValue) {
@@ -31,12 +36,14 @@ inputs.forEach(input => input.addEventListener('keyup', (e) => {
             }
             break;
         case 2:
+            cardNumberLength = inputValue.length;
             if (Number(inputValue) || !inputValue) {
                 cardText[inputNumber - 2].textContent = inputValue
                 if (!inputValue) {
                     cardText[inputNumber - 2].textContent = '0000 0000 0000 0000'
                     flag = false
                 }
+
             } else {
                 inputs[inputNumber - 1].style.outline = '1px solid red'
                 flag = true
@@ -70,6 +77,7 @@ inputs.forEach(input => input.addEventListener('keyup', (e) => {
             }
             break;
         case 5:
+            cvcLength = inputValue.length;
             if (Number(inputValue) || !inputValue) {
                 wrongFormulas[3].textContent = 'Wrong format'
                 cardText[inputNumber - 1].textContent = inputValue
@@ -102,6 +110,7 @@ inputs.forEach(input => input.addEventListener('keyup', (e) => {
     } else if (flag == false) {
         if (inputNumber == 4) {
             inputs[inputNumber - 1].style.outline = '1px solid rgb(211, 211, 211)'
+            inputs[inputNumber - 2].style.outline = '1px solid rgb(211, 211, 211)'
             wrongFormulas[inputNumber - 2].style.opacity = '0';
 
         } else if (inputNumber == 5) {
@@ -120,6 +129,27 @@ inputs.forEach(input => input.addEventListener('keyup', (e) => {
 
 
 btns.forEach(btn => btn.addEventListener('click', () => {
+    if (nameLength < 3) {
+        inputs[0].style.outline = '1px solid red'
+        inputs[0].nextElementSibling.textContent = 'Name is too short'
+        inputs[0].nextElementSibling.style.opacity = '1'
+        flag = true
+    }
+
+    if (cardNumberLength < 16) {
+        inputs[1].style.outline = '1px solid red'
+        inputs[1].nextElementSibling.textContent = 'Card number is too short'
+        inputs[1].nextElementSibling.style.opacity = '1'
+        flag = true
+    }
+
+    if (cvcLength < 3) {
+        inputs[4].style.outline = '1px solid red'
+        inputs[4].nextElementSibling.textContent = 'CVC number is too short'
+        inputs[4].nextElementSibling.style.opacity = '1'
+        flag = true
+    }
+
 
     emptyInputs = inputs.filter(input => input.value.length == 0)
     inputs.forEach(input => {
@@ -127,6 +157,7 @@ btns.forEach(btn => btn.addEventListener('click', () => {
             flag = true;
         }
     })
+
     if (emptyInputs.length == 0 && flag === false) {
         console.log('completeState')
         startState.classList.toggle('noActive')
@@ -140,6 +171,7 @@ btns.forEach(btn => btn.addEventListener('click', () => {
             emptyInput.nextElementSibling.style.opacity = "1"
         })
     }
+
 
     if (completeState.classList.length == 2) {
         btn.textContent = 'Continue'
